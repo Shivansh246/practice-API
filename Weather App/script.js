610c0def0a54e8eb721bd1f6a4d2fbadc4f1c4d0
Shivansh246 <285635402+Shivansh246@users.noreply.github.com>
2026-09-05 16:28:57 +0530

    render data using weather api

---
+++ Weather App/script.js
@@ -0,0 +1,58 @@
+
+const Inputlocation = document.querySelector("#InputLocation");
+const LocationSubmitBtn = document.querySelector("#LocationSubmitBtn");
+const info=document.querySelector("#info");
+
+function renderData(address,temperature,conditions,feelslike,humidity,windSpeed){
+    const addressDiv=document.createElement("div");
+    const temperatureDiv=document.createElement("div");
+    const conditionsDiv=document.createElement("div");
+    const feelslikeDiv=document.createElement("div");
+    const humidityDiv=document.createElement("div");
+    const windSpeedDiv=document.createElement("div");
+    let deg='F';
+    let temp = temperature;
+
+    addressDiv.textContent=`Address: ${address}`;
+    temperatureDiv.textContent=`Temperature: ${temp}&deg;F`;
+    conditionsDiv.textContent=`Conditions: ${conditions}`;
+    feelslikeDiv.textContent=`Feelslike: ${feelslike}`;
+    humidityDiv.textContent=`Humidity: ${humidity}`;
+    windSpeedDiv.textContent=`WindSpeed: ${windSpeed}`;
+
+    info.appendChild(addressDiv);
+    info.appendChild(temperatureDiv);
+    info.appendChild(conditionsDiv);
+    info.appendChild(feelslikeDiv);
+    info.appendChild(humidityDiv);
+    info.appendChild(windSpeedDiv);
+    info.appendChild(toggleBtn);
+}
+async function getData(givenLocation){
+    try{
+        const response= await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${givenLocation}?key=YDSZM2UZ8BWQ68SZFD5KMCMCS`);
+        const usefulData = await getUsefulData(response);
+        renderData(usefulData.address,usefulData.temperature,usefulData.conditions,usefulData.feelslike,usefulData.humidity,usefulData.windSpeed,usefulData.toggleBtn);
+    }catch(error){
+        console.log(error);
+    }
+}
+
+async function getUsefulData(response){
+    try{
+        const data = await response.json(); 
+        return {address:data.resolvedAddress,
+                temperature: data.currentConditions.temp,
+                conditions: data.currentConditions.conditions,
+                feelslike: data.currentConditions.feelslike,
+                humidity: data.currentConditions.humidity,
+                windSpeed: data.currentConditions.windSpeed
+        };
+    }catch(error){
+        console.log(error);
+    }
+}
+LocationSubmitBtn.addEventListener("click",(e)=>{
+    renderData(Inputlocation.value);
+
+})
