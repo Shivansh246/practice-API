53705f361a665a2e92caa21449dcc6917313ffe3
Shivansh246 <285635402+Shivansh246@users.noreply.github.com>
2026-09-05 16:54:09 +0530

    fix bug, wrong temp calculated

--- Weather App/script.js
+++ Weather App/script.js
@@ -3,7 +3,7 @@ const Inputlocation = document.querySelector("#InputLocation");
 const LocationSubmitBtn = document.querySelector("#LocationSubmitBtn");
 const info=document.querySelector("#info");
 
-function renderData(address,temperature,conditions,feelslike,humidity,windSpeed){
+function renderData(address,temperature,conditions,feelslike,humidity,windSpeed,deg){
     info.textContent="";
     const addressDiv=document.createElement("div");
     const temperatureDiv=document.createElement("div");
@@ -11,11 +11,9 @@ function renderData(address,temperature,conditions,feelslike,humidity,windSpeed)
     const feelslikeDiv=document.createElement("div");
     const humidityDiv=document.createElement("div");
     const windSpeedDiv=document.createElement("div");
-    let deg='F';
-    let temp = temperature;
 
     addressDiv.textContent=`Address: ${address}`;
-    temperatureDiv.textContent=`Temperature: ${temp}&#176;F`;
+    temperatureDiv.textContent=`Temperature: ${temperature}°${deg}`;
     conditionsDiv.textContent=`Conditions: ${conditions}`;
     feelslikeDiv.textContent=`Feelslike: ${feelslike}`;
     humidityDiv.textContent=`Humidity: ${humidity}`;
@@ -24,15 +22,15 @@ function renderData(address,temperature,conditions,feelslike,humidity,windSpeed)
     const toggleBtn = document.createElement("button");
     toggleBtn.textContent="Toggle Temperature";
     toggleBtn.addEventListener("click",(e)=>{
-        let newTemp=temp;
+        let newTemp=temperature;
         if(deg==='F'){
             deg='C'
-            newTemp = (temp - 32) * 5/9;
+            newTemp = (temperature - 32) * 5/9;
         }else{
             deg='F';
-            newTemp = (temp * 9/5) + 32;
+            newTemp = (temperature * 9/5) + 32;
         }
-        renderData(address,newTemp,conditions,feelslike,humidity,windSpeed);
+        renderData(address,newTemp,conditions,feelslike,humidity,windSpeed,deg);
     });
 
     info.appendChild(addressDiv);
@@ -47,7 +45,7 @@ async function getData(givenLocation){
     try{
         const response= await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${givenLocation}?key=YDSZM2UZ8BWQ68SZFD5KMCMCS`);
         const usefulData = await getUsefulData(response);
-        renderData(usefulData.address,usefulData.temperature,usefulData.conditions,usefulData.feelslike,usefulData.humidity,usefulData.windspeed,usefulData.toggleBtn);
+        renderData(usefulData.address,usefulData.temperature,usefulData.conditions,usefulData.feelslike,usefulData.humidity,usefulData.windspeed,'F');
     }catch(error){
         console.log(error);
     }
@@ -69,5 +67,4 @@ async function getUsefulData(response){
 }
 LocationSubmitBtn.addEventListener("click",(e)=>{
     getData(Inputlocation.value);
-
 })
